c94d5dff739026435bdf3da49d74bbc14754bd21
Rishi <rishirschouhan@gmail.com>
2023-11-14 14:57:21 +0530

    Basic app structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=''>Hello World!!</h1>
+      <UilReact size={40}/>
+    </div>
+  );
+}
+
+export default App;
